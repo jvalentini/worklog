37328ec20b4cb108a86f17b706146e05a2ca23c0
Justin Valentini <justin.valentini@gmail.com>
2026-01-05 17:26:44 -0500

    fix: remove unused import in config.test.ts

--- src/utils/config.test.ts
+++ src/utils/config.test.ts
@@ -1,5 +1,5 @@
 import { afterEach, describe, expect, test } from "bun:test";
-import { mkdir, rm, writeFile } from "node:fs/promises";
+import { mkdir, rm } from "node:fs/promises";
 import { tmpdir } from "node:os";
 import { join } from "node:path";
 import { expandPath, loadConfig } from "../utils/config.ts";
